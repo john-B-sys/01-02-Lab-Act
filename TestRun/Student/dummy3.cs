dad22dd5569dc8285fa68923b9902eee008cdb24
john-B-sys <johnbernie44@gmail.com>
2025-08-07 12:50:25 +0800

    Add files via upload

---
+++ TestRun/Student/dummy3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRun.Student
+{
+    internal abstract class Student
+    {
+        public abstract void Run();
+
+            Console.WriteLine("Dummy code...");
+        
+     }
+}
